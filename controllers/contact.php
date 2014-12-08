25051a15713d7ffef61fd13212a80a63c5eaa246
pg2g12 <paul@ug-vpn65.ecs.soton.ac.uk>
2014-12-08 22:06:38 +0000

    Added email validation to contact form

--- controllers/contact.php
+++ controllers/contact.php
@@ -5,11 +5,17 @@ class Contact extends Controller {
 	public function index($f3) {
 		if($this->request->is('post')) {
 			extract($this->request->data);
+
+			if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+
 			$from = "From: $from";
-
 			mail($to,$subject,$message,$from);
-
 			StatusMessage::add('Thank you for contacting us');
+
+			} else {
+
+			StatusMessage::add('Invalid email','danger');
+			}
 			return $f3->reroute('/');
 		}	
 	}
